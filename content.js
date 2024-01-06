49428a2f435b67bda88403003187b03855336f32
sucks555 <102584033+sucks-555@users.noreply.github.com>
2024-01-07 03:48:38 +0900

    Update content.js

--- content.js
+++ content.js
@@ -6,13 +6,24 @@ if (location.host === "www.pixiv.net") {
     button && button.click();
     await new Promise(resolve => {
       setTimeout(() => {
-        const figure = document.querySelectorAll("figure")[0];
-        const image = figure.querySelectorAll("img");
-        for (const img of image) {
-          const pathArray = img.src.split('/');
-          const parts = pathArray[pathArray.length - 1].split('.') || [`${location.pathname.split("/artworks/")[1]}_p${i}`];
-          const extension = parts.length > 1 ? parts[parts.length - 1] : 'jpg';
-          filelist.push({ url: img.src, fileName: parts[0], extension: extension });
+        const exception = document.querySelector(".sc-1qlsbpb-0");
+        if (exception) {
+          const lane = exception.querySelector('.sc-1oz5uvo-1').querySelectorAll('img');
+          for (const img of lane) {
+            const pathArray = img.src.split('/');
+            const parts = pathArray[pathArray.length - 1].split('.') || [`${location.pathname.split("/artworks/")[1]}_p${i}`];
+            const extension = parts.length > 1 ? parts[parts.length - 1] : 'jpg';
+            filelist.push({ url: img.src, fileName: parts[0], extension: extension });
+          }
+        } else {
+          const figure = document.querySelectorAll("figure")[0];
+          const image = figure.querySelectorAll("img");
+          for (const img of image) {
+            const pathArray = img.src.split('/');
+            const parts = pathArray[pathArray.length - 1].split('.') || [`${location.pathname.split("/artworks/")[1]}_p${i}`];
+            const extension = parts.length > 1 ? parts[parts.length - 1] : 'jpg';
+            filelist.push({ url: img.src, fileName: parts[0], extension: extension });
+          }
         }
         resolve();
       }, 1000);
@@ -21,27 +32,56 @@ if (location.host === "www.pixiv.net") {
   }
 
   async function FullGETpixiv() {
-    const wait = 150;
+    const wait = 200;
     const filelist = [];
     const button = document.querySelector(".sc-emr523-2");
     button && button.click();
     await new Promise(resolve => {
       setTimeout(async () => {
-        const figure = document.querySelectorAll("figure")[0];
-        const a = figure.querySelectorAll("a");
-        for (let i = 0; i < a.length; i++) {
-          a[i].click();
-          await delay(wait);
-          const presentation = document.querySelector(`.sc-1pkrz0g-1`);
-          const image = presentation.querySelector("img");
-          const pathArray = image.src.split('/');
-          const parts = pathArray[pathArray.length - 1].split('.') || [`${location.pathname.split("/artworks/")[1]}_p${i}`];
-          const extension = parts.length > 1 ? parts[parts.length - 1] : 'jpg';
-          filelist.push({ url: image.src, fileName: parts[0], extension: extension });
-          await delay(wait);
-          const nextButton = document.querySelector("button.sc-691snt-1.sc-691snt-3.cdYVyZ.bThEqM");
-          nextButton && nextButton.click();
+        const exception = document.querySelector(".sc-1qlsbpb-0");
+        if (exception) {
+          const lane = exception.querySelector('.sc-1oz5uvo-1').querySelectorAll('img');
+          const count = parseInt(document.querySelector(".sc-yzbnij-6").innerHTML, 10)-1 || lane.length
+          const master_image = exception.querySelector("img")
+          master_image && master_image.click()
+          for (let i = 0; i < count; i ++) {
+            await delay(wait);
+            const origin = document.querySelector(".sc-tmsb78-0");
+            const image = origin.querySelector("img")
+            const pathArray = image.src.split('/');
+            const parts = pathArray[pathArray.length - 1].split('.') || [`${location.pathname.split("/artworks/")[1]}_p${i}`];
+            const extension = parts.length > 1 ? parts[parts.length - 1] : 'jpg';
+            filelist.push({ url: image.src, fileName: parts[0], extension: extension });
+            await delay(wait);
+            image && image.scrollIntoView({
+              behavior: 'instant',
+              block: 'end'
+            });
+            setTimeout(() => {
+              const nextButton = document.querySelector(".sc-tmsb78-3");
+              nextButton && nextButton.click();
+            }, 0);
+          }
+
+        } else {
+
+          const figure = document.querySelectorAll("figure")[0];
+          const a = figure.querySelectorAll("a");
+          for (let i = 0; i < a.length; i++) {
+            a[i].click();
+            await delay(wait);
+            const presentation = document.querySelector(`.sc-1pkrz0g-1`);
+            const image = presentation.querySelector("img");
+            const pathArray = image.src.split('/');
+            const parts = pathArray[pathArray.length - 1].split('.') || [`${location.pathname.split("/artworks/")[1]}_p${i}`];
+            const extension = parts.length > 1 ? parts[parts.length - 1] : 'jpg';
+            filelist.push({ url: image.src, fileName: parts[0], extension: extension });
+            await delay(wait);
+            const nextButton = document.querySelector("button.sc-691snt-1.sc-691snt-3.cdYVyZ.bThEqM");
+            nextButton && nextButton.click();
+          }
         }
+
         resolve();
       }, 1000);
     });
